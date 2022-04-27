97c422b6067566587449bb58fd292df62d8b4a53
sharminakter017 <mssharminakter2000@gmail.com>
2022-04-27 19:54:46 +0600

    upload all files

---
+++ app.js
@@ -0,0 +1,424 @@
+
+const studentdata = [
+
+    {
+        name     :  'Asraful',
+        roll     : 1,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Robin',
+        roll     : 2,
+        age      : 15,
+        class    : 3,
+        gender   : 'male',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Ashik',
+        roll     : 3,
+        age      : 22,
+        class    : 5,
+        gender   : 'male',
+        location : 'Banani',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Maliha',
+        roll     : 4,
+        age      : 15,
+        class    : 2,
+        gender   : 'Female',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Borna',
+        roll     : 5,
+        age      : 10,
+        class    : 2,
+        gender   : 'Female',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Sahida',
+        roll     : 6,
+        age      : 15,
+        class    : 5,
+        gender   : 'Female',
+        location : 'Banani',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Firoz',
+        roll     : 7,
+        age      : 20,
+        class    : 5,
+        gender   : 'male',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Jidan',
+        roll     : 8,
+        age      : 22,
+        class    : 3,
+        gender   : 'male',
+        location : 'Banani',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Kawsar',
+        roll     : 9,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Asraful',
+        roll     : 10,
+        age      : 10,
+        class    : 2,
+        gender   : 'male',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Somrat',
+        roll     : 11,
+        age      : 15,
+        class    : 3,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Hossain',
+        roll     : 12,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Mariya',
+        roll     : 13,
+        age      : 22,
+        class    : 3,
+        gender   : 'Female',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Mahi',
+        roll     : 14,
+        age      : 15,
+        class    : 2,
+        gender   : 'Female',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Sumon',
+        roll     : 15,
+        age      : 25,
+        class    : 5,
+        gender   : 'male',
+        location : 'Banani',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Akas',
+        roll     : 16,
+        age      : 10,
+        class    : 3,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Mosiur',
+        roll     : 17,
+        age      : 25,
+        class    : 5,
+        gender   : 'male',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Sakil',
+        roll     : 18,
+        age      : 10,
+        class    : 2,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Sagor',
+        roll     : 19,
+        age      : 15,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Bappi',
+        roll     : 20,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Minhaj',
+        roll     : 21,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Foysal',
+        roll     : 22,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Nasir',
+        roll     : 23,
+        age      : 15,
+        class    : 5,
+        gender   : 'male',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Irfan',
+        roll     : 24,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Akhi',
+        roll     : 25,
+        age      : 10,
+        class    : 3,
+        gender   : 'Female',
+        location : 'Banani',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Bristi',
+        roll     : 26,
+        age      : 22,
+        class    : 5,
+        gender   : 'Female',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Sharmin',
+        roll     : 27,
+        age      : 10,
+        class    : 2,
+        gender   : 'Female',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Sakib',
+        roll     : 28,
+        age      : 10,
+        class    : 5,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Milon',
+        roll     : 29,
+        age      : 15,
+        class    : 5,
+        gender   : 'male',
+        location : 'Uttora',
+        addmission_fees : 500
+    },
+    {
+        name     :  'Imran',
+        roll     : 30,
+        age      : 10,
+        class    : 3,
+        gender   : 'male',
+        location : 'Dhaka',
+        addmission_fees : 500
+    }
+
+
+
+
+
+];
+
+
+let total = 0;
+
+studentdata.map((data,index) => {
+
+    console.log(`
+
+      ${index + 1}. Name : ${data.name} addmission fees : ${data.addmission_fees}
+    
+    
+
+
+    `)
+
+    total += data.addmission_fees;
+
+
+});
+
+console.log(`Total addmission fees : ${total}`);
+
+
+
+
+// find all female student
+
+studentdata.forEach((data,index) => {
+
+    if (data.gender == 'Female'){
+        console.log(`
+
+          Name              : ${data.name}
+          roll              : ${data.roll}
+          age               : ${data.age}
+          class             : ${data.class}
+          gender            : ${data.gender}
+          location          : ${data.location}
+          addmission fees   : ${data.addmission_fees}
+        
+        
+        `)
+    }
+
+
+
+
+
+
+});
+
+
+// class wise student result
+
+for(data of studentdata){
+
+    if (data.class == 5){
+        console.log(`
+
+          name : ${data.name}
+          age  : ${data.age}
+          roll : ${data.roll}
+          class : ${data.class}
+          location : ${data.location}
+        
+        
+        `)
+    }
+
+
+
+};
+
+
+for(data of studentdata){
+
+    if (data.class == 3){
+        console.log(`
+
+          name : ${data.name}
+          age  : ${data.age}
+          roll : ${data.roll}
+          class : ${data.class}
+          location : ${data.location}
+        
+        
+        `)
+    }
+
+
+
+};
+
+
+
+// location wise result
+
+studentdata.forEach((data,index) => {
+
+    if (data.location == 'Uttora' || data.location == 'Banani'){
+
+        console.log(`
+          
+          Id    : ${index + 1}
+          Name  : ${data.name}
+          roll  : ${data.roll}
+          class : ${data.class}
+          location : ${data.location}
+        
+        
+        `)
+
+    }
+
+
+
+});
+
+
+// age 10 to 25 student find 
+
+for (data of studentdata){
+
+    if (data.age > 10 && data.age <= 25){
+        console.log(`
+
+         ===========================>
+
+         name  : ${data.name}
+         age   : ${data.age}
+         class : ${data.class}
+         roll  : ${data.roll}
+
+        
+        `);
+    };
+};
+
+
+
+
+
+
+
+
